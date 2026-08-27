8c3784fc35915a3a2cebe6a4f538732d3d5df233
Steven IMANZI <stivenimanzi1@gmail.com>
2026-08-27 22:12:48 +0200

    add location and phone fields to branches table and implement CRUD controller functionality

--- apps/api/database/migrations/2026_08_27_201216_add_details_to_branches_table.php
+++ apps/api/database/migrations/2026_08_27_201216_add_details_to_branches_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('branches', function (Blueprint $table) {
-            //
+            $table->string('location')->nullable();
+            $table->string('phone')->nullable();
         });
     }
 
